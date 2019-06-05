e08dee540d8e08fdfaf6f88e1e35b59311bc27b9
Penguin1212 <51422957+Penguin1212@users.noreply.github.com>
2019-06-05 18:27:17 -0400

    Update example_cog.py

--- cogs/example_cog.py
+++ cogs/example_cog.py
@@ -10,7 +10,7 @@ class Mod (commands.Cog):
 # look at other cogs in this folder for more examples
 
     @commands.command(pass_context=True)
-    async def test(self, ctx):
+    async def example(self, ctx):
         print("Yes you just used a cog! now make your own")
 
 # copy above to make a new command or just use the above to make a new command
