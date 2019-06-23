ba8c35e0e8de528333d01c3238116a46dfc2a8d5
Penguin1212 <51422957+Penguin1212@users.noreply.github.com>
2019-06-23 15:43:45 -0400

    NEW
    new version (4.2)

---
+++ start.bat
@@ -0,0 +1,11 @@
+python -m pip install --upgrade pip setuptools wheel
+pip install urbandictionary
+pip install -U discord.py[voice]
+pip install configparser
+pip install praw
+pip install urllib3
+pip install youtube-dl
+pip install --upgrade youtube-dl
+pip install spotdl
+pip install termcolor
+python Bot_Spud.py
